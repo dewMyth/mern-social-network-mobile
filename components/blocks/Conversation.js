0d9be25611d2d9eecba58e329daed9cdd5addd66
Dewmith Akalanka <akalankadewmith@gmail.com>
2022-11-28 15:28:14 +0530

    Add chat component

--- components/blocks/Conversation.js
+++ components/blocks/Conversation.js
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 
-import {Avatar, Image} from 'react-native-paper';
+import {Avatar, Image, Text} from 'react-native-paper';
 
 import GlobalState from '../../GlobalState';
 
+import {timeAgo} from '../../helpers/timeAgo';
+
 const Conversation = ({conversation}) => {
+  console.log(conversation);
   const user = GlobalState.user;
   const [friend, setFriend] = useState(null);
+  const [lastMsg, setLastMsg] = useState(null);
 
   useEffect(() => {
     const friendId = conversation.members.find(friend => friend !== user._id);
@@ -20,7 +24,6 @@ const Conversation = ({conversation}) => {
           },
         }).then(response => {
           return response.json().then(data => {
-            console.log(data);
             setFriend(data);
           });
         });
@@ -31,61 +34,71 @@ const Conversation = ({conversation}) => {
     getFriend();
   }, [conversation, user]);
 
+  useEffect(() => {
+    const getLastMessage = async () => {
+      try {
+        await fetch(baseUrl + '/message/get-messages/' + conversation._id, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+          },
+        }).then(response => {
+          response.json().then(data => {
+            // console.log('msgs from =>', data[data.length - 1].text);
+            setLastMsg(data[data.length - 1].text);
+          });
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getLastMessage();
+  }, [conversation]);
+
   return (
     <>
-      <TouchableOpacity>
-        <Text>Conversation List Render Here</Text>
+      <TouchableOpacity style={styles.conversationContainer}>
+        {friend && (
+          <>
+            <View style={styles.avatarContainer}>
+              <Avatar.Image
+                size={40}
+                source={
+                  friend.profilePicture
+                    ? {uri: friend.profilePicture}
+                    : {uri: 'https://i.imgur.com/AZr24sM.jpg'}
+                }
+              />
+            </View>
+            <View style={styles.titleContainer}>
+              <Text variant="titleMedium">
+                {friend.firstName + ' ' + friend.lastName}
+              </Text>
+              <Text variant="labelMedium" style={{color: 'gray'}}>
+                {lastMsg ? lastMsg : 'No messages yet...'}
+                {/* {timeAgo(new Date(conversation.updatedAt))} */}
+              </Text>
+            </View>
+          </>
+        )}
       </TouchableOpacity>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    margin: 20,
-  },
-
-  conversationList: {
-    display: 'flex',
-    marginVertical: 20,
-    flex: 1,
-  },
-
   conversationContainer: {
     display: 'flex',
     flexDirection: 'row',
-    marginVertical: 20,
+    flex: 1,
     alignItems: 'center',
   },
-
   avatarContainer: {
     flex: 1,
   },
 
   titleContainer: {
     flex: 5,
-  },
-
-  fab: {
-    position: 'absolute',
-    margin: 16,
-    right: 0,
-    bottom: 0,
-    backgroundColor: '#da0037',
-  },
-
-  modal: {
-    display: 'flex',
-    margin: 20,
-    backgroundColor: 'white',
-    padding: 20,
-  },
-
-  friend: {
-    display: 'flex',
-    margin: 10,
   },
 });
 
